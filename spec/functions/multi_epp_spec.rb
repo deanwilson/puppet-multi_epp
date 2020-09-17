c7736b7a0bb0530c03e3b721123b34f00d0feedd
Dean Wilson <dean.wilson@gmail.com>
2020-09-17 08:43:11 +0100

    Fix RSpec/ExampleWording violations
    Remove the should and reword to be more active in the messages

--- spec/functions/multi_epp_spec.rb
+++ spec/functions/multi_epp_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe 'multi_epp' do
   describe 'when no values are provided' do
-    it 'should raise an ArgumentError' do
+    it 'raises an ArgumentError' do
       is_expected.to run.with_params.and_raise_error(ArgumentError, /least one/)
     end
   end
 
   describe 'when no templates are found' do
-    it 'should raise an ArgumentError' do
+    it 'raises an ArgumentError' do
       is_expected.to run.with_params(
         ['fakefile.epp', 'anotherfakefile.epp']
       ).and_raise_error(ArgumentError, /No match.+anotherfakefile.epp/)
@@ -17,7 +17,7 @@ describe 'multi_epp' do
 
   # this is essentially testing the underlying function, which isn't great.
   describe 'when an invalid templates is found' do
-    it 'should raise an ArgumentError' do
+    it 'raises an ArgumentError' do
       is_expected.to run.with_params(
         ['single_match/invalid_name.epp']
       ).and_raise_error(ArgumentError, /Syntax error/)
@@ -27,7 +27,7 @@ describe 'multi_epp' do
   ################## Valid usages
 
   describe 'when passed a valid epp template' do
-    it 'should expand it' do
+    it 'expands it' do
       is_expected.to run.with_params(
         ['single_match/hello.epp']
       ).and_return(/Hello EPP world/)
@@ -35,7 +35,7 @@ describe 'multi_epp' do
   end
 
   describe 'when passed a valid epp template and a value' do
-    it 'should expand the epp template' do
+    it 'expands the epp template' do
       is_expected.to run.with_params(
         ['single_match/dynamic_name.epp'], 'name' => 'Dean'
       ).and_return(/Hello Dean/)
@@ -43,7 +43,7 @@ describe 'multi_epp' do
   end
 
   describe 'when passed multi missing and a valid epp template' do
-    it 'should skip the missing and expand the valid epp template' do
+    it 'skips the missing files and expands the valid epp template' do
       is_expected.to run.with_params(
         ['notreal/doesntexist.epp', 'single_match/hello.epp', 'notreal/doesntexist2.epp']
       ).and_return(/Hello EPP world/)
